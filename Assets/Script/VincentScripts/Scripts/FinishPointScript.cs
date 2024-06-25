6cda7a23dd9263f6f747805f2fcc530f73b8891c
LAPTOP-LPQ843GE <hydropumpppp.012@gmail.com>
2024-06-25 11:13:15 +0800

    062524 Updates: Editting Script FinishPoint

--- Assets/Script/VincentScripts/Scripts/FinishPointScript.cs
+++ Assets/Script/VincentScripts/Scripts/FinishPointScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishPointScript : MonoBehaviour
 {
@@ -13,6 +14,19 @@ public class FinishPointScript : MonoBehaviour
         {
             gameCompletion.SetActive(true);
             Time.timeScale = 1f;
+            UnlockNewLevel();
+        }
+    }
+
+    void UnlockNewLevel()
+    {
+        if(SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnclockLevel1", PlayerPrefs.GetInt("UnlockedLevel1", 1));
+            PlayerPrefs.Save();
+
+
         }
     }
 }
