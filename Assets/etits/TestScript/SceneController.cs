a198556671d5709f8fb90e573497ffe4bb4b97a0
LAPTOP-LPQ843GE <hydropumpppp.012@gmail.com>
2024-06-26 21:06:58 +0800

    062624 Updates: Working on Level 1 Animations and Levels (Incomplete)

---
+++ Assets/etits/TestScript/SceneController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements.Experimental;
+
+public class SceneController : MonoBehaviour
+{
+    
+    public static SceneController instance;
+
+    [SerializeField] Animator transitionAnim;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void NextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+    
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);  
+    }
+
+}
