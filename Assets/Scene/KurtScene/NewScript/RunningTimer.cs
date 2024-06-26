2946f41e81cab461b51339091756c6f9368e1e58
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-06-26 09:32:35 -0700

    06/26/2024 Updates: WORKING ON PH1

--- Assets/Scene/KurtScene/NewScript/RunningTimer.cs
+++ Assets/Scene/KurtScene/NewScript/RunningTimer.cs
@@ -7,17 +7,16 @@ using TMPro;
 
 public class RunningTimer : MonoBehaviour
 {
-
     [SerializeField] TextMeshProUGUI timerTxt;
-
     [SerializeField] TextMeshProUGUI textComplete;
- 
-    float elapsedTime;
+    [SerializeField] TextMeshProUGUI textCompletion1;
+    [SerializeField] TextMeshProUGUI textCompletion2;
+    [SerializeField] TextMeshProUGUI textCompletion3;
 
     [SerializeField] private Pause pauseMenu;
 
+    float elapsedTime;
     public bool isPicked = false;
-    //string pepe = "0:10";
   
     void Update()
     {
@@ -30,11 +29,29 @@ public class RunningTimer : MonoBehaviour
                 int seconds = Mathf.FloorToInt(elapsedTime % 60);
                 timerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
-                /*if(timerTxt.text == string.Format("{0:00}:{0:05}", minutes, seconds))     //FUTURE CODE NEEDED FOR ACQUIRING THE SET TIMER
+                if(timerTxt.text == string.Format("{0:00}:{0:31}", minutes, seconds))
+                {                   
+                    string text1 = "100 score 30 sec.";
+                    string weight1 = "#44";
+                    textCompletion1.text = "<alpha=" + weight1 + ">" + text1;
+
+                    //textCompletion1.text = "<font-weight=" +"\""+ weight1 +"\""+ ">" + text1 + "</font-weight>";
+                }
+
+                else if (timerTxt.text == string.Format("{0:00}:{0:46}", minutes, seconds))
                 {
-                Debug.Log("super10");
-                Time.timeScale = 0;
-                }*/
+                    string text2 = "80 score 50 sec.";
+                    string weight1 = "#44";
+                    textCompletion2.text = "<alpha=" + weight1 + ">" + text2;
+                }
+
+                else if (timerTxt.text == string.Format("{0:00}:{1:00}", minutes, seconds))
+                {
+                    string text3 = "50 score 60 sec.";
+                    string weight1 = "#44";
+                    textCompletion2.text = "<alpha=" + weight1 + ">" + text3;
+                }
+
             }
             else 
             {
