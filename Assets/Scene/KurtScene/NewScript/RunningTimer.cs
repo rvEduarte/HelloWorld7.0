751c7883008f6dc772a9b96cd4b3bcfeba04175f
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-06-25 23:33:01 -0700

    06/25/2024 Updates: WORKING ON PH1

--- Assets/Scene/KurtScene/NewScript/RunningTimer.cs
+++ Assets/Scene/KurtScene/NewScript/RunningTimer.cs
@@ -29,17 +29,19 @@ public class RunningTimer : MonoBehaviour
                 int minutes = Mathf.FloorToInt(elapsedTime / 60);
                 int seconds = Mathf.FloorToInt(elapsedTime % 60);
                 timerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+                /*if(timerTxt.text == string.Format("{0:00}:{0:05}", minutes, seconds))     //FUTURE CODE NEEDED FOR ACQUIRING THE SET TIMER
+                {
+                Debug.Log("super10");
+                Time.timeScale = 0;
+                }*/
             }
             else 
             {
                 textComplete.text = timerTxt.text;
             }
       
-            /*if(timerTxt.text == string.Format("{0:00}:{0:05}", minutes, seconds))     //FUTURE CODE NEEDED FOR ACQUIRING THE SET TIMER
-            {
-                Debug.Log("super10");
-                Time.timeScale = 0;
-            }*/
+            
         }
     }
 }
