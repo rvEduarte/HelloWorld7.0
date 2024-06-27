44f3b4dac937e22319e68c8ce69f388c404638b7
LAPTOP-LPQ843GE <hydropumpppp.012@gmail.com>
2024-06-27 14:44:24 +0800

    062724 Updates: Finishit

--- Assets/Script/VincentScripts/Scripts/FinishPointScript.cs
+++ Assets/Script/VincentScripts/Scripts/FinishPointScript.cs
@@ -6,14 +6,28 @@ using UnityEngine.SceneManagement;
 public class FinishPointScript : MonoBehaviour
 {
 
-    [SerializeField] GameObject gameCompletion;
+    //[SerializeField] GameObject gameCompletion;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            gameCompletion.SetActive(true);
+            // gameCompletion.SetActive(true);
             Time.timeScale = 1f;
+            UnlockNewLevel();
+            //SceneController.instance.NextLevel();
+        }
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnclockLevel1", PlayerPrefs.GetInt("UnlockedLevel1", 1));
+            PlayerPrefs.Save();
+
+
         }
     }
 }
